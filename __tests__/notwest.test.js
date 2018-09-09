7d5ba1627578ec91449dbf60b7abb347ef298727
Jonny Edge <jonnyedge@outlook.com>
2018-09-09 17:12:50 +0100

    Revisiting Week 3 code using the learnings from Week 4 to set up and properly test it, making sure it actually did what it needed to do – which it has done, handily.

--- __tests__/notwest.test.js
+++ __tests__/notwest.test.js
@@ -4,12 +4,52 @@ const {
     withdraw,
     checkBalance,
     viewStatement,
+    transactions,
+    deposits,
+    withdrawals,
+    startingBalance,
+    viewDeposits,
+    viewWithdrawals
   } = require('../src/notwest');
 
 describe('deposit', () => {
-    it('adds the amount passed into the function to the balance property within the currentAccount object', () => {
-      expect(deposit(100)).toEqual({
-        balance: 100,
-      });
+    it('adds amount passed into the function to the balance property within the currentAccount object', () => {
+      deposit(100)
+      expect(currentAccount[0].balance).toEqual(200)
     });
 });
+
+describe('withdraw', () => {
+  it('removes amount passed into the function from the balance property within the currentAccount object', () => {
+    withdraw(100)
+    expect(currentAccount[0].balance).toEqual(100)
+  })
+})
+
+describe('checkBalance', () => {
+  it ('returns the value of the balance property within the currentAccount object', () => {
+    const result = checkBalance()
+    expect(result).toEqual('Your balance is: £' + currentAccount[0].balance)
+  })
+})
+
+describe ('viewStatement', () => {
+  it ('returns a list of transactions carried out up to this point', () => {
+  const result = viewStatement()
+  expect(result).toEqual('Hi there, ' + currentAccount[0].name + '! Your balance was: £' + startingBalance + '. You made the following transactions: ' + (transactions.join(", ")) + '. Of which the following were deposits: ' + (deposits.join(", ")) + '. And the following were withdrawals: ' + (withdrawals.join(", ")) + ". Your balance is: £" + currentAccount[0].balance)
+  })
+})
+
+describe ('viewDeposits', () => {
+  it ('returns a list of deposits, and deposits only, carried out up to this point', () => {
+    const result = viewDeposits()
+    expect(result).toEqual('You made the following deposits: ' + (deposits.join(', ')))
+  })
+})
+
+describe ('viewWithdrawals', () => {
+  it ('returns a list of withdrawals, and withdrawals only, carried out up to this point', () => {
+    const result = viewWithdrawals()
+    expect(result).toEqual('You made the following withdrawals: ' + (withdrawals.join(', ')))
+  })
+})
--- src/notwest.js
+++ src/notwest.js
@@ -27,20 +27,30 @@ const checkBalance = () => {
 }
 
 const viewStatement = () => {
-    console.log('Hi there, ' + currentAccount[0].name + '!')
-    console.log('Your balance was: £' + startingBalance)
-    console.log('You made the following transactions: ' + (transactions.join(", ")))
-    console.log('Of which the following were deposits: ' + (deposits.join(", ")))
-    console.log('And the following were withdrawals: ' + (withdrawals.join(", ")))
-    console.log('Your balance is: £' + currentAccount[0].balance)
+    return 'Hi there, ' + currentAccount[0].name + '! Your balance was: £' + startingBalance + '. You made the following transactions: ' + (transactions.join(", ")) + '. Of which the following were deposits: ' + (deposits.join(", ") + '. And the following were withdrawals: ' + (withdrawals.join(", ")) + ". Your balance is: £" + currentAccount[0].balance)
+    
+    // console.log('Hi there, ' + currentAccount[0].name + '!')
+    // console.log('Your balance was: £' + startingBalance)
+    // console.log('You made the following transactions: ' + (transactions.join(", ")))
+    // console.log('Of which the following were deposits: ' + (deposits.join(", ")))
+    // console.log('And the following were withdrawals: ' + (withdrawals.join(", ")))
+    // console.log('Your balance is: £' + currentAccount[0].balance)
 }
 
-deposit(100)
-withdraw(100)
-deposit(150)
-deposit(25)
-withdraw(15)
-viewStatement()
+const viewDeposits = () => {
+    return 'You made the following deposits: ' + (deposits.join(', '))
+}
+
+const viewWithdrawals = () => {
+    return 'You made the following withdrawals: ' + (withdrawals.join(', '))
+}
+
+// deposit(100)
+// withdraw(100)
+// deposit(150)
+// deposit(25)
+// withdraw(15)
+// viewStatement()
 
 module.exports = {
     currentAccount,
@@ -48,4 +58,10 @@ module.exports = {
     withdraw,
     checkBalance,
     viewStatement,
+    transactions,
+    deposits,
+    withdrawals,
+    startingBalance,
+    viewDeposits,
+    viewWithdrawals
 }
